4442ce098eefb822be5c10178a51f79c6ca0a92a
JohnRamos14 <Ramos.Johnmba@gmail.com>
2022-12-11 13:14:12 -0800

    deploy site

--- src/TrackSearchResult.js
+++ src/TrackSearchResult.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
-export default function TrackSearchResult({track, chooseTrack}) {
-    function handlePlay() {
-        chooseTrack(track)
-    }
+export default function TrackSearchResult({track}) {
   return (
-    <div className="d-flex m-2 align-items-center" style={{cursor: "pointer"}}
-    onClick={handlePlay}>
-        <img src={track.albumUrl} style={{height: "64px", width: "64px"}} />
+    <div className="d-flex m-2 align-items-center" style={{cursor: "pointer"}}>
+        <img src={track.albumUrl} alt="..." style={{height: "64px", width: "64px"}} />
         <div className="ml-5">
             <div>{track.title}</div>
             <div className="text-muted">{track.artist}</div>
--- src/useAuth.js
+++ src/useAuth.js
@@ -1,51 +1,46 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 export default function useAuth(code) {
-  const [accessToken, setAccessToken] = useState();
-  const [refreshToken, setRefreshToken] = useState();
-  const [expiresIn, setExpiresIn] = useState();
+    const [accessToken, setAccessToken] = useState();
+    // const [refreshToken, setRefreshToken] = useState();
+    // const [expiresIn, setExpiresIn] = useState();
 
-  useEffect(() => {
-    axios
-      .post("http://localhost:3001/login", {
-        code,
-      })
-      .then((res) => {
-        setAccessToken(res.data.accessToken);
-        setRefreshToken(res.data.refreshToken);
-        setExpiresIn(res.data.expiresIn);
-        console.log(res.data);
-        // this is to get rid of the code in the url and just show localhost:3000
-        window.history.pushState({}, null, "/");
-      })
-      .catch(() => {
-        window.location = "/";
-      });
-  }, [code]);
+    useEffect(() => {
+        axios.post('http://localhost:3001/login', {
+            code,}).then(res => {
+                setAccessToken(res.data.accessToken);
+                // setRefreshToken(res.data.refreshToken);
+                // setExpiresIn(res.data.expiresIn);
+                console.log(res.data)
+                // this is to get rid of the code in the url and just show localhost:3000
+                // window.history.pushState({}, null, '/')
+            }).catch((err)=>{
+                // window.location = '/'
+                console.log(err)
+            })
 
-  // this is to refresh the token so that user doesn't have to log in again after 1 hour
-  useEffect(() => {
-    if (!refreshToken || !expiresIn) return;
-    const interval = setInterval(() => {
-      axios
-        .post("http://localhost:3001/refresh", {
-          refreshToken,
-        })
-        .then((res) => {
-          setAccessToken(res.data.accessToken);
-          setExpiresIn(res.data.expiresIn);
-          console.log(res.data);
-          // this is to get rid of the code in the url and just show localhost:3000
-          window.history.pushState({}, null, "/");
-        })
-        .catch(() => {
-          window.location = "/";
-        });
-    }, (expiresIn - 60) * 1000);
-    // this is to clear the timeout so that it doesn't keep refreshing the token
-    return () => clearInterval(interval);
-  }, [refreshToken, expiresIn]);
+    }, [code])
 
-  return accessToken;
+
+// this is to refresh the token so that user doesn't have to log in again after 1 hour
+//     useEffect(() => {
+//         if (!refreshToken || !expiresIn) return
+//         const interval = setInterval(() => {
+//             axios.post('http://localhost:3001/refresh', {
+//                 refreshToken,}).then(res => {
+//                     setAccessToken(res.data.accessToken);
+//                     setExpiresIn(res.data.expiresIn);
+//                     console.log(res.data)
+//                     // this is to get rid of the code in the url and just show localhost:3000
+//                     window.history.pushState({}, null, '/')
+//                 }).catch(()=>{
+//                     window.location = '/'
+//                 })
+//         }, (expiresIn - 60) * 1000)
+// // this is to clear the timeout so that it doesn't keep refreshing the token
+//       return () => clearInterval(interval)
+//     }, [refreshToken, expiresIn])
+
+    return accessToken;
 }
