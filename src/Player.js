3140e0231c0e84b080a3ecceeb4bfe202f4e0351
JohnRamos14 <Ramos.Johnmba@gmail.com>
2022-12-11 11:45:53 -0800

    first commit

---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SpotifyPlayer from 'react-spotify-web-playback'
+
+export default function Player(accessToken, trackUri) {
+    if (!accessToken) return null
+  return <SpotifyPlayer
+  token={accessToken}
+  showSaveIcon
+  //alway expect an array to be passed in
+  uris={trackUri ? [trackUri] : []}
+  />
+}
